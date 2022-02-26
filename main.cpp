b693f7d2b08b054dae7048c2fc38898313ec1ca3
Christian <caldanas2@miumg.edu.gt>
2022-02-26 11:48:27 -0600

    Operacion Division Correcta

--- main.cpp
+++ main.cpp
@@ -59,7 +59,16 @@ int main(int argc, char** argv) {
                 getch(); //Detener el Programa
                 break;
             }
+            case '/':{ //Caso de Division
+                cout<<"		<--- Division --->"<<endl<<endl;
+                cout<<"Digite el Operando 1: "; cin>>op1;
+                cout<<"Digite el Operando 2: "; cin>>op2;
+                resul = op1 / op2; //Operación de Division
 
+                cout<<"La division resulta ser: "<<resul;
+                getch(); //Detener el progrma
+                break;
+            }
             default: { //Mensaje de Error
                 cout<<"Este Operador es Invalido, Preciona enter para continuar";
                 getch(); //Detener el programa
